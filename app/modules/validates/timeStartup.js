b6c80901fc644fee36bd2b946e68e521cfdc5a5a
Higor Diego <higordiegoti@gmail.com>
2018-01-20 22:52:40 -0300

    alterando retorno de body

--- app/modules/validates/timeStartup.js
+++ app/modules/validates/timeStartup.js
@@ -3,12 +3,12 @@ module.exports = app => {
     const Validate = require('../../helpers/validate')
     return {
         create: (req, res, next) => {
-            const required = ['time_id', 'time_startup_id']
+            const required = ['time_id', 'startup_id']
             const error = Validate.requestRequired(req, required, Errors)
             error ? res.status(400).json(error) : next()
         },
         update: (req, res, next) => {
-            const required = ['time_id', 'time_startup_id']
+            const required = ['time_id', 'startup_id']
             const error = Validate.requestOptional(req, required, Errors)
             error ? res.status(400).json(error) : next()
         },
